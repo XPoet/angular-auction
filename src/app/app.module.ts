a3b45ac8caf180029635712e7cf72ec7b5c715f2
zhangshiliu <zhangshiliu@metled.com.cn>
2018-04-28 09:26:59 +0800

    已完成产品组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SearchComponent } from './components/search/search.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ProductComponent } from './components/product/product.component';
+import { StarsComponent } from './components/stars/stars.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductComponent,
+    StarsComponent
   ],
   imports: [
     BrowserModule
